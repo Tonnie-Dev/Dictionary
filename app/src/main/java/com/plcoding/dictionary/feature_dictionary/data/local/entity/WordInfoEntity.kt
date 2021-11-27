cdcd148411444f5173ced4c58ea7cb15989f1134
Antony <vontonnie@gmail.com>
2021-11-27 19:17:40 +0300

    Create the DAO class

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/local/entity/WordInfoEntity.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/local/entity/WordInfoEntity.kt
@@ -3,6 +3,7 @@ package com.plcoding.dictionary.feature_dictionary.data.local.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.plcoding.dictionary.feature_dictionary.domain.model.Meaning
+import com.plcoding.dictionary.feature_dictionary.domain.model.WordInfo
 
 @Entity(tableName = "wordinfoentity")
 data class WordInfoEntity(val meanings: List<Meaning>,
@@ -13,5 +14,14 @@ data class WordInfoEntity(val meanings: List<Meaning>,
                           @PrimaryKey
 val id:Int? = null){
 
-    //helper 
+    //helper method to convert entity to the model class
+    fun toWordInfo():WordInfo{
+
+        return WordInfo(
+            meanings = meanings,
+            origin = origin,
+            phonetic = phonetic,
+            word = word
+        )
+    }
 }
