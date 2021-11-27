1e8f70062f5b76ea7f278763a8493d67303d3b27
Antony <vontonnie@gmail.com>
2021-11-27 16:28:33 +0300

    Add @GET Request

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/DictionaryAPI.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/DictionaryAPI.kt
@@ -1,2 +1,10 @@
 package com.plcoding.dictionary.feature_dictionary.data.remote
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DictionaryAPI {
+
+    @GET(value = "/api/v2/entries/en/{word}")
+    suspend fun getWordInfo(@Path(value = "word") word:String)
+}
