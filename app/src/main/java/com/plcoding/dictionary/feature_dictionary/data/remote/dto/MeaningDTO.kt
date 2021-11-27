3272a36e0fb86a00b712eb8c654e538ffa5cfa15
Antony <vontonnie@gmail.com>
2021-11-27 18:20:24 +0300

    Add the Meaning Class domain object

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/dto/MeaningDTO.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/dto/MeaningDTO.kt
@@ -1,10 +1,17 @@
 package com.plcoding.dictionary.feature_dictionary.data.remote.dto
 
 
+import com.plcoding.dictionary.feature_dictionary.domain.model.Meaning
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class MeaningDTO(
     val definitions: List<DefinitionDTO>,
     val partOfSpeech: String
-)
+){
+
+    fun toMeaning():Meaning{
+
+        return Meaning(definitions = definitions.map { it.toDefinition() }, partOfSpeech = partOfSpeech)
+    }
+}
