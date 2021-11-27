f7c0c59e9948c680f2fe4e5b17ae872184aa6178
Antony <vontonnie@gmail.com>
2021-11-27 18:51:46 +0300

    Create the Entity Class

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/local/entity/WordInfoEntity.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/local/entity/WordInfoEntity.kt
@@ -1,3 +1,17 @@
 package com.plcoding.dictionary.feature_dictionary.data.local.entity
 
-data class WordInfoEntity()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.plcoding.dictionary.feature_dictionary.domain.model.Meaning
+
+@Entity(tableName = "wordinfoentity")
+data class WordInfoEntity(val meanings: List<Meaning>,
+                          val origin: String,
+                          val phonetic: String,
+                          val word: String,
+
+                          @PrimaryKey
+val id:Int? = null){
+
+    //helper 
+}
