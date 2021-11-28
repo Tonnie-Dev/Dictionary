679dc6da0b377a3e573abe12ef2f1efd5435def6
Antony <vontonnie@gmail.com>
2021-11-28 07:04:25 +0300

    Add Repo definition

---
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/domain/repository/WordInfoInterface.kt
@@ -0,0 +1,16 @@
+package com.plcoding.dictionary.feature_dictionary.domain.repository
+
+import com.plcoding.dictionary.core.util.Resource
+import com.plcoding.dictionary.feature_dictionary.domain.model.WordInfo
+import kotlinx.coroutines.flow.Flow
+
+interface WordInfoInterface {
+
+
+    /*We use a flow to emit multiple values over a perion of time
+    * we fist  emit the loading status
+    * then we get the actual WordInfo from cache
+    * we can then make the request when we get the response we can emit
+    * another list of WordInfo gotten from the API*/
+    fun getWordInfo(word:String): Flow<Resource<List<WordInfo>>>
+}
