3b76059df1bc3e3af056259ba5ba29f669c1d685
Antony <vontonnie@gmail.com>
2021-11-28 15:43:28 +0300

    Create the WordInfoRepositoryImpl

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/repository/WordInfoRepositoryImpl.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/repository/WordInfoRepositoryImpl.kt
@@ -1,3 +1,23 @@
 package com.plcoding.dictionary.feature_dictionary.data.repository
 
-class WordInfoRepositoryImpl : Wor
+import com.plcoding.dictionary.core.util.Resource
+import com.plcoding.dictionary.feature_dictionary.data.local.WordDAO
+import com.plcoding.dictionary.feature_dictionary.data.remote.DictionaryAPI
+import com.plcoding.dictionary.feature_dictionary.domain.model.WordInfo
+import com.plcoding.dictionary.feature_dictionary.domain.repository.WordInfoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class WordInfoRepositoryImpl(private val dao: WordDAO, private val api: DictionaryAPI) : WordInfoRepository {
+    override fun getWordInfo(word: String): Flow<Resource<List<WordInfo>>> = flow {
+
+
+        //we first emit loading when we start the request to display progress bar
+
+        emit(Resource.Loading())
+
+        //get data from the database
+
+        val wordInfos = dao
+    }
+}
