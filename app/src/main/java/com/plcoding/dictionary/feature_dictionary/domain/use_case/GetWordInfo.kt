3f295f0f6084216b5bde84bc631d2093e638fb06
Antony <vontonnie@gmail.com>
2021-11-29 21:23:05 +0300

    Add the Use case code

---
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/domain/use_case/GetWordInfo.kt
@@ -0,0 +1,24 @@
+package com.plcoding.dictionary.feature_dictionary.domain.use_case
+
+import com.plcoding.dictionary.core.util.Resource
+import com.plcoding.dictionary.feature_dictionary.domain.model.WordInfo
+import com.plcoding.dictionary.feature_dictionary.domain.repository.WordInfoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetWordInfo(private val repo: WordInfoRepository) {
+
+
+    operator fun  invoke(word:String): Flow<Resource<List<WordInfo>>> {
+
+        //validate word
+
+        if (word.isBlank()){
+            //return empty flow so that no API call takes place
+            return flow {  }
+        }
+
+        //else get word info
+        return repo.getWordInfo(word)
+    }
+}
