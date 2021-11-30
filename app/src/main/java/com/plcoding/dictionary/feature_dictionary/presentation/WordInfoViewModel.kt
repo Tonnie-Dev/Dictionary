b926b23c429189045aaa52ba4d4451a3484835b3
Antony <vontonnie@gmail.com>
2021-11-30 07:29:10 +0300

    Create States for the ViewModel

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/WordInfoViewModel.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/WordInfoViewModel.kt
@@ -1,11 +1,23 @@
 package com.plcoding.dictionary.feature_dictionary.presentation
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.plcoding.dictionary.feature_dictionary.domain.use_case.GetWordInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class WordInfoViewModel @Inject constructor(private val useCase:GetWordInfo) :
+class WordInfoViewModel @Inject constructor(private val useCase: GetWordInfo) :
     ViewModel() {
+
+    //STATES
+    var searchQuery = mutableStateOf("")
+        private set
+
+    var state = mutableStateOf(WordInfoState())
+        private set
+
+    //EVENT FLOW
+
+    
 }
