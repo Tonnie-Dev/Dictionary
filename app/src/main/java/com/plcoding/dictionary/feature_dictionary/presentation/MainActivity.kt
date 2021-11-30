e17f727920ce4a956147344d474dbbe341568e3a
Antony <vontonnie@gmail.com>
2021-11-30 08:42:34 +0300

    Move UI package to the Presentation Package

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/MainActivity.kt
@@ -1,9 +1,9 @@
-package com.plcoding.dictionary
+package com.plcoding.dictionary.feature_dictionary.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.plcoding.dictionary.ui.theme.DictionaryTheme
+import com.plcoding.dictionary.feature_dictionary.presentation.ui.theme.DictionaryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/ui/theme/Shape.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.plcoding.dictionary.ui.theme
+package com.plcoding.dictionary.feature_dictionary.presentation.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
