0532afb12aa497137be5f64e3ded23cd60e4993e
Antony <vontonnie@gmail.com>
2021-11-30 20:44:06 +0300

    Apply LaunchedEffect to collect latest events

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/MainActivity.kt
@@ -3,14 +3,61 @@ package com.plcoding.dictionary.feature_dictionary.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.plcoding.dictionary.feature_dictionary.presentation.ui.theme.DictionaryTheme
+import kotlinx.coroutines.flow.collectLatest
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DictionaryTheme {
-                
+
+
+                //get reference to viewModel, state and scaffold state
+
+                val viewModel: WordInfoViewModel = hiltViewModel()
+                val state by viewModel.state
+                val scaffoldState = rememberScaffoldState()
+
+                /*This will be needed to actually listen to events from the
+                event flow that we have inside the ViewModel*/
+
+                LaunchedEffect(key1 = true, block = {
+
+                    viewModel.eventFlow.collectLatest {
+
+                        event ->
+
+                        when(event){
+
+
+                            is WordInfoViewModel.UIEvent.ShowSnackbar -> {
+
+                                scaffoldState.snackbarHostState.showSnackbar(event.message)
+                            }
+                        }
+                    }
+                })
+
+
+                Scaffold(scaffoldState = scaffoldState) {
+
+
+                    Box(modifier = Modifier.fillMaxSize()){
+
+
+
+                    }
+                }
+
             }
         }
     }
