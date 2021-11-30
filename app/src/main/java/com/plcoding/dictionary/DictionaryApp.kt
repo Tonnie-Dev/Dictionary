704b24ef441670e232719136a03dd16d9ae07697
Antony <vontonnie@gmail.com>
2021-11-30 08:48:56 +0300

    Initialize Timber

--- app/src/main/java/com/plcoding/dictionary/DictionaryApp.kt
+++ app/src/main/java/com/plcoding/dictionary/DictionaryApp.kt
@@ -2,7 +2,13 @@ package com.plcoding.dictionary
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class DictionaryApp : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
 }
