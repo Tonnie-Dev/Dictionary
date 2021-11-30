b1719811a4e55785bf3054ef3150598e508da2a2
Antony <vontonnie@gmail.com>
2021-11-30 19:50:19 +0300

    @Provides Repository Injection

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/di/AppModule.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/di/AppModule.kt
@@ -1,6 +1,9 @@
 package com.plcoding.dictionary.feature_dictionary.di
 
 import androidx.room.RoomDatabase
+import com.plcoding.dictionary.feature_dictionary.data.local.WordDAO
+import com.plcoding.dictionary.feature_dictionary.data.remote.DictionaryAPI
+import com.plcoding.dictionary.feature_dictionary.data.repository.WordInfoRepositoryImpl
 import com.plcoding.dictionary.feature_dictionary.domain.repository.WordInfoRepository
 import com.plcoding.dictionary.feature_dictionary.domain.use_case.GetWordInfo
 import dagger.Module
@@ -15,12 +18,24 @@ import javax.inject.Singleton
 
 object AppModule {
 
-
+//USE CASE
     @Provides
     @Singleton
     fun providesUseCase(repository: WordInfoRepository): GetWordInfo{
 
         return GetWordInfo(repo = repository)
+    }
+
+
+    //REPOSITORY
+
+    @Provides
+    @Singleton
+
+    fun providesRepository(dao: WordDAO, api: DictionaryAPI):WordInfoRepository{
+
+
+        return WordInfoRepositoryImpl(dao, api)
     }
 }
 
