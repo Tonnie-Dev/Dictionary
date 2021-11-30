1b47338745820d13ae92918b3ef0f9b244408a1e
Antony <vontonnie@gmail.com>
2021-11-30 07:40:51 +0300

    Add Event Flow for the ViewModel

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/WordInfoViewModel.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/WordInfoViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.plcoding.dictionary.feature_dictionary.domain.use_case.GetWordInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,7 +18,14 @@ class WordInfoViewModel @Inject constructor(private val useCase: GetWordInfo) :
     var state = mutableStateOf(WordInfoState())
         private set
 
+    var eventFlow = MutableSharedFlow<UIEvent>()
+    private set
+    
+
     //EVENT FLOW
 
-    
-}
+sealed class UIEvent{
+
+    data class ShowSnackbar(val message:String):UIEvent()
+
+}}
