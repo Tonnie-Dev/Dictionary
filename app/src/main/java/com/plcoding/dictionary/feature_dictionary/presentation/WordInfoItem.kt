3520fb7e22db5c52c6b9cd0527b420c7c93ca373
Antony <vontonnie@gmail.com>
2021-12-01 07:46:16 +0300

    Add the column contain for WordInfo

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/WordInfoItem.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/WordInfoItem.kt
@@ -1,8 +1,22 @@
 package com.plcoding.dictionary.feature_dictionary.presentation
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.plcoding.dictionary.feature_dictionary.domain.model.WordInfo
+
 
 @Composable
-fun WordInfoItem() {
+fun WordInfoItem(wordInfo:WordInfo, modifier: Modifier = Modifier) {
+    
+    
+    Column(modifier = modifier) {
+
+
+        
+        
+    }
+
+    
     
 }
