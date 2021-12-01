c154f413e6dd92a0f9ebcabde4eff58a55874fbe
Antony <vontonnie@gmail.com>
2021-12-01 07:51:07 +0300

    Add word header text

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/WordInfoItem.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/WordInfoItem.kt
@@ -1,22 +1,30 @@
 package com.plcoding.dictionary.feature_dictionary.presentation
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.plcoding.dictionary.feature_dictionary.domain.model.WordInfo
 
 
 @Composable
-fun WordInfoItem(wordInfo:WordInfo, modifier: Modifier = Modifier) {
-    
-    
+fun WordInfoItem(wordInfo: WordInfo, modifier: Modifier = Modifier) {
+
+
     Column(modifier = modifier) {
 
+        //Word Header
+        Text(
+            text = wordInfo.word,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
 
-        
-        
     }
 
-    
-    
+
 }
