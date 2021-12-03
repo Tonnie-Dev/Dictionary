2dff04d0064bba98f76df8116c502fb2714a112c
Antony <vontonnie@gmail.com>
2021-12-03 08:14:56 +0300

    Remove unused imports from the App Module

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/di/AppModule.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/di/AppModule.kt
@@ -1,29 +1,21 @@
 package com.plcoding.dictionary.feature_dictionary.di
 
 import android.app.Application
-import android.content.Context
 import androidx.room.Room
-import androidx.room.RoomDatabase
 import com.google.gson.Gson
 import com.plcoding.dictionary.feature_dictionary.data.local.Converters
-import com.plcoding.dictionary.feature_dictionary.data.local.WordDAO
 import com.plcoding.dictionary.feature_dictionary.data.local.WordInfoDatabase
 import com.plcoding.dictionary.feature_dictionary.data.local.util.GsonParser
-import com.plcoding.dictionary.feature_dictionary.data.local.util.MoshiParser
 import com.plcoding.dictionary.feature_dictionary.data.remote.DictionaryAPI
 import com.plcoding.dictionary.feature_dictionary.data.repository.WordInfoRepositoryImpl
 import com.plcoding.dictionary.feature_dictionary.domain.repository.WordInfoRepository
 import com.plcoding.dictionary.feature_dictionary.domain.use_case.GetWordInfo
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -31,7 +23,7 @@ import javax.inject.Singleton
 
 
 object AppModule {
-   
+
 
     init {
 
