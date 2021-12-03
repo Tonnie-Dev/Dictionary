5f5007fe376d4625c99c9f356f0324a3824d2926
Antony <vontonnie@gmail.com>
2021-12-03 08:20:17 +0300

    Create up MeaningDTO Class

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/dto/DefinitionDTO.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/dto/DefinitionDTO.kt
@@ -2,10 +2,8 @@ package com.plcoding.dictionary.feature_dictionary.data.remote.dto
 
 
 import com.plcoding.dictionary.feature_dictionary.domain.model.Definition
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
+
 data class DefinitionDTO(
     val antonyms: List<String>,
     val definition: String,
--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/dto/MeaningDTO.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/dto/MeaningDTO.kt
@@ -2,9 +2,8 @@ package com.plcoding.dictionary.feature_dictionary.data.remote.dto
 
 
 import com.plcoding.dictionary.feature_dictionary.domain.model.Meaning
-import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
+
 data class MeaningDTO(
     val definitions: List<DefinitionDTO>,
     val partOfSpeech: String
