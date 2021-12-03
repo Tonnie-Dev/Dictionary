214eab386d372d4a46aa169a4404f01569b94d72
Antony <vontonnie@gmail.com>
2021-12-03 08:20:43 +0300

    Create up PhoneticDTO Class

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/dto/PhoneticDTO.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/dto/PhoneticDTO.kt
@@ -2,10 +2,8 @@ package com.plcoding.dictionary.feature_dictionary.data.remote.dto
 
 
 import com.plcoding.dictionary.feature_dictionary.domain.model.Phonetic
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
+
 data class PhoneticDTO(
     val audio: String,
     val text: String
