5845cbf3d08493d35d86c3c5c276aee0c53f2bc7
Antony <vontonnie@gmail.com>
2021-12-05 08:04:00 +0300

    Implement MyConverter Class

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/local/util/MyConverter.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/local/util/MyConverter.kt
@@ -1,4 +1,27 @@
 package com.plcoding.dictionary.feature_dictionary.data.local.util
 
+import com.plcoding.dictionary.feature_dictionary.domain.model.Meaning
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
 class MyConverter {
+
+
+
+    private val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    private val jsonAdapter: JsonAdapter<Meaning> = moshi.adapter(Meaning::class.java)
+    fun fromJson(json: String):Meaning?{
+
+      return  jsonAdapter.fromJson(json)
+
+    }
+    fun toJson(meaning: Meaning):String?{
+
+
+
+
+        return jsonAdapter.toJson(meaning)
+    }
+
 }
