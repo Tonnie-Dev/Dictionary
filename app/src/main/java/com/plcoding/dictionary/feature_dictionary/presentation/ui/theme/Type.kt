b4688238493490dc55a0d496de98f69e68d14d7d
Antony <vontonnie@gmail.com>
2021-12-08 19:22:30 +0300

    Initialize Font Family

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/ui/theme/Type.kt
@@ -1,10 +1,28 @@
 package com.plcoding.dictionary.feature_dictionary.presentation.ui.theme
 
+
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+
 import androidx.compose.ui.text.font.FontWeight
+
+
 import androidx.compose.ui.unit.sp
+import com.plcoding.dictionary.R
+
+//initialize font family - FontFamily takes a list of fonts
+
+private val RobotoCondensed = FontFamily(
+    Font(resId = R.font.roboto_condensed_light, weight = FontWeight.W300),
+    Font(resId = R.font.roboto_condensed_light_italic, weight = FontWeight.W300),
+    Font(resId = R.font.roboto_condensed_regular, weight = FontWeight.W400),
+    Font(resId = R.font.roboto_condensed_bold, weight = FontWeight.W700)
+    )
+
+
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
