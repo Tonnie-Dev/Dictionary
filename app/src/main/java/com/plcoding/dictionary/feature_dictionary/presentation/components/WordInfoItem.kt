d49f31cf98d4e37c60cfe2fc48010bd3bce3e23f
Antony <vontonnie@gmail.com>
2021-12-09 08:29:15 +0300

    Draw Flow Row inside WordInfoItem

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/components/WordInfoItem.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/components/WordInfoItem.kt
@@ -2,6 +2,7 @@ package com.plcoding.dictionary.feature_dictionary.presentation
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -11,7 +12,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.accompanist.flowlayout.FlowRow
 import com.plcoding.dictionary.feature_dictionary.domain.model.WordInfo
+import com.plcoding.dictionary.feature_dictionary.presentation.components.SynonymTag
 
 
 @Composable
@@ -30,7 +33,7 @@ fun WordInfoItem(wordInfo: WordInfo, modifier: Modifier = Modifier) {
         )
 
         Spacer(modifier = Modifier.height(8.dp))
-        
+
 
         //loop over the meanings
 
@@ -38,7 +41,7 @@ fun WordInfoItem(wordInfo: WordInfo, modifier: Modifier = Modifier) {
 
 
             //part of speech e.g. noun, adjective, verb etc
-            
+
             Text(text = meaning.partOfSpeech, style = MaterialTheme.typography.h6)
 
             //loop over the meaning of the word
@@ -51,24 +54,45 @@ fun WordInfoItem(wordInfo: WordInfo, modifier: Modifier = Modifier) {
                 Spacer(modifier = Modifier.height(8.dp))
 
                 //loop over examples
-                
-                definition.example?.let { 
-                    
-                    example ->
-                    
+
+                definition.example?.let {
+
+                        example ->
+
                     Text(text = "Example: $example")
                 }
                 Spacer(modifier = Modifier.height(8.dp))
 
-                definition.synonyms.forEach {
+                Text(text = "Synonyms", style = MaterialTheme.typography.h6)
+                Spacer(modifier = Modifier.height(4.dp))
 
-                    Text(text = "Syn: $it")
+                FlowRow(
+                    mainAxisSpacing = 10.dp,
+                    crossAxisSpacing = 10.dp,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    definition.synonyms.forEach { synonym ->
+
+                        if (synonym.isEmpty()) {
+                            Text(text = "No Synonyms Found", style = MaterialTheme.typography.body1)
+
+                        } else {
+
+                            SynonymTag(synonym = synonym)
+                            //Text(text = "Syn: $it")
+
+                        }
+
+
+                    }
+
 
                 }
+
+
+
             }
             Spacer(modifier = Modifier.height(8.dp))
-
-
 
 
         }
