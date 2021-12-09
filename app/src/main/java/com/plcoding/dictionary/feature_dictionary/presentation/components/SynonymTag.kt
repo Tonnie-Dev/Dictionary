db5b7f240a769ac9f006e0fd320606cf82677c7a
Antony <vontonnie@gmail.com>
2021-12-09 08:08:16 +0300

    Add SynonymTag

---
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/components/SynonymTag.kt
@@ -0,0 +1,33 @@
+package com.plcoding.dictionary.feature_dictionary.presentation.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SynonymTag(synonym:String) {
+
+    Box(modifier = Modifier.border(
+        width = 1.dp,
+        color = MaterialTheme.colors.secondary,
+        shape = RoundedCornerShape(75.dp)
+    )) {
+
+
+        Text(
+            text = synonym,
+            style = MaterialTheme.typography.overline,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(5.dp)
+        )
+    }
+
+
+}
