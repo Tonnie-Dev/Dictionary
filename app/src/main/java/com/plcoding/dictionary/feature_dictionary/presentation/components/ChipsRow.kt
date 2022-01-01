f7a134f168f048508c829588eed71f26e3ff6be8
Antony <vontonnie@gmail.com>
2022-01-01 07:12:10 +0300

    Add WordChip composable

---
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/components/ChipsRow.kt
@@ -0,0 +1,11 @@
+package com.plcoding.dictionary.feature_dictionary.presentation.components
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WordChip(word:String) {
+    Text(text = word )
+}
+
+
