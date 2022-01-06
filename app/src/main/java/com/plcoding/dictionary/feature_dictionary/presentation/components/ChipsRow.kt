3fe4f7fd65c6d46659a837e7e64e7c1b0c1d42ec
Antony <vontonnie@gmail.com>
2022-01-06 18:40:23 +0300

    Add Surface for the Chip Item

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/components/ChipsRow.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/components/ChipsRow.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Row
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.plcoding.dictionary.feature_dictionary.domain.model.WordInfo
 
 @Composable
@@ -14,7 +18,22 @@ fun WordChip(
     word: String,
     onClickChip: (String) -> Unit
 ) {
-    Text(text = word, modifier = Modifier.clickable { onClickChip(word) })
+
+    //use surface for elevation
+
+    Surface(
+        elevation = 8.dp,
+        color = MaterialTheme.colors.primary,
+        shape = MaterialTheme.shapes.small
+    ) {
+
+        Text(
+            text = word,
+            modifier = Modifier.clickable { onClickChip(word) }
+        )
+
+    }
+
 }
 
 @Composable
@@ -26,18 +45,18 @@ fun WordChipsRow(
 
     Row(modifier = Modifier.horizontalScroll(scrollState)) {
 
-      /*  for (wordInfo in words){
+        /*  for (wordInfo in words){
 
-            WordChip(word = wordInfo.word, onClickChip = onClickChip)
+              WordChip(word = wordInfo.word, onClickChip = onClickChip)
 
-        }*/
-
-
-        for (i in 1..10){
+          }*/
 
 
-WordChip(word = "Text $i ", onClickChip = {})
-    }
+        for (i in 1..10) {
+
+
+            WordChip(word = "Text $i ", onClickChip = {})
+        }
 
     }
 
