6ed3c712dcb55aa480f7685d675424a41a95ed59
Antony <vontonnie@gmail.com>
2022-01-08 16:48:36 +0300

    Add ShowShimmerCard Animation

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/show_word_info/components/ShimmerCardItem.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/show_word_info/components/ShimmerCardItem.kt
@@ -2,10 +2,12 @@ package com.plcoding.dictionary.feature_dictionary.presentation.show_word_info.c
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.unit.dp
+import com.plcoding.dictionary.feature_dictionary.presentation.anim.ShimmerAnimation
 
 @Composable
 fun ShimmerCardItem(brush: Brush) {
@@ -29,4 +31,21 @@ fun ShimmerCardItem(brush: Brush) {
                     .background(brush = brush)
         )
     }
+}
+
+
+@Composable
+fun ShowShimmerCard() {
+    
+    
+    LazyColumn(content = {
+        
+        item { 
+            
+            ShimmerAnimation {
+                brush -> ShimmerCardItem(brush = brush) 
+            }
+        }
+    })
+    
 }
