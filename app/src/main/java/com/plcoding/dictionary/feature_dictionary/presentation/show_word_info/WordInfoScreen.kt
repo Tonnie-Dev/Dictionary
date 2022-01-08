12e4ff12ea863d00a283323cf46488c6fb3b4458
Antony <vontonnie@gmail.com>
2022-01-08 10:06:33 +0300

    Add Scaffold to the WordInfoScreen

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/show_word_info/WordInfoScreen.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/show_word_info/WordInfoScreen.kt
@@ -1,16 +1,21 @@
 package com.plcoding.dictionary.feature_dictionary.presentation.show_word_info
 
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.plcoding.dictionary.feature_dictionary.presentation.utils.UIEvent.OnHideKeyboard
+import com.plcoding.dictionary.feature_dictionary.presentation.utils.UIEvent.OnShowSnackbar
 
 @ExperimentalComposeUiApi
 @Composable
-fun WordInfoScreen(viewModel:WordInfoViewModel = hiltViewModel()) {
+fun WordInfoScreen(viewModel: WordInfoViewModel = hiltViewModel()) {
 
     //States
     val wordInfoState by viewModel.wordInfoState
@@ -18,5 +23,42 @@ fun WordInfoScreen(viewModel:WordInfoViewModel = hiltViewModel()) {
     val scaffoldState = rememberScaffoldState()
     val keyboardController = LocalSoftwareKeyboardController.current
     val scrollState = rememberScrollState()
+
+
+    /*Launched Effect listen to events from the
+     event flow that we have inside the ViewModel*/
+    LaunchedEffect(key1 = true, block = {
+
+
+        val uiEvent = viewModel.uiEvent.collect{ event ->
+
+            when (event){
+
+                is OnHideKeyboard -> {
+                    keyboardController?.hide()
+
+                }
+                is OnShowSnackbar -> {
+
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = event.msg,
+                        actionLabel = event.action
+
+                    )
+
+                }
+            }
+
+        }
+    })
+
+    Scaffold(scaffoldState = scaffoldState, topBar = {}) {
+
+   Surface() {
+            
+
+        }
+    }
+
 
 }
