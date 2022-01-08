cf984967d2225a33f7980d03a467f1ab52a90bf7
Antony <vontonnie@gmail.com>
2022-01-08 09:46:31 +0300

    Add state variables to WordInfoScreen

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/show_word_info/WordInfoScreen.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/show_word_info/WordInfoScreen.kt
@@ -1,8 +1,22 @@
 package com.plcoding.dictionary.feature_dictionary.presentation.show_word_info
 
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.hilt.navigation.compose.hiltViewModel
 
+@ExperimentalComposeUiApi
 @Composable
-fun WordInfoScreen() {
+fun WordInfoScreen(viewModel:WordInfoViewModel = hiltViewModel()) {
+
+    //States
+    val wordInfoState by viewModel.wordInfoState
+    val lastTenWords by viewModel.lastTenWords
+    val scaffoldState = rememberScaffoldState()
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val scrollState = rememberScrollState()
 
 }
