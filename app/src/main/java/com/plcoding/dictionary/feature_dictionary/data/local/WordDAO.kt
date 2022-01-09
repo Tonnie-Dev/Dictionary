b715dd7dfb34bbb1d73e65fd23b469a846763243
Antony <vontonnie@gmail.com>
2022-01-09 20:03:32 +0300

    Add getTheLastSearchWord dao query

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/local/WordDAO.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/local/WordDAO.kt
@@ -5,26 +5,26 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.plcoding.dictionary.feature_dictionary.data.local.entity.WordInfoEntity
-import kotlinx.coroutines.flow.Flow
+
 @Dao
 interface WordDAO {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertWordInfos(infos:List<WordInfoEntity>)
+    suspend fun insertWordInfos(infos: List<WordInfoEntity>)
 
     @Query("DELETE FROM wordinfoentity WHERE word IN (:words)")
-    suspend fun deleteWordInfos(words:List<String>)
+    suspend fun deleteWordInfos(words: List<String>)
 
 
     // Pipe || is for String concatenation '%' is a string placeholder
     @Query("SELECT * FROM wordinfoentity WHERE word LIKE '%' || :word || '%'")
-    suspend fun getWordInfos(word:String): List<WordInfoEntity>
+    suspend fun getWordInfos(word: String): List<WordInfoEntity>
 
 
-   /* @Query("SELECT DISTINCT word,meanings FROM wordinfoentity ORDER BY id DESC LIMIT 10")*/
+    /* @Query("SELECT DISTINCT word,meanings FROM wordinfoentity ORDER BY id DESC LIMIT 10")*/
     @Query("SELECT word, meanings FROM wordinfoentity GROUP BY word ORDER BY id DESC LIMIT 20")
-    suspend fun getLastTenWords():List<WordInfoEntity>
+    suspend fun getLastTenWords(): List<WordInfoEntity>
 
-
-    suspend fun getTheLastSearchWord():WordInfoEntity?
+    @Query("SELECT * FROM wordinfoentity ORDER BY id DESC LIMIT 1")
+    suspend fun getTheLastSearchWord(): WordInfoEntity
 }
