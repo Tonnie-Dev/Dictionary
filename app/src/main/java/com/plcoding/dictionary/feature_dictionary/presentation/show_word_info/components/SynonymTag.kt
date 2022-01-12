7e3d9030bde7dfda52e5a678ed5f617960f99c55
Antony <vontonnie@gmail.com>
2022-01-12 08:22:30 +0300

    Replace Box Tax with Surface

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/show_word_info/components/SynonymTag.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/show_word_info/components/SynonymTag.kt
@@ -3,32 +3,60 @@ package com.plcoding.dictionary.feature_dictionary.presentation.show_word_info.c
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun SynonymTag(synonym:String, onClickTag:(String)->Unit) {
+fun SynonymTag(synonym: String, onClickTag: (String) -> Unit) {
 
-    Box(modifier = Modifier.border(
-        width = 1.dp,
-        color = MaterialTheme.colors.primary,
-        shape = RoundedCornerShape(100.dp)
-    ).clickable {  onClickTag(synonym)}) {
+/* Box(modifier = Modifier
+            .border(
+                width = 1.5.dp,
+                color = MaterialTheme.colors.primary,
+                shape = RoundedCornerShape(20.dp)
+            )
+            .clickable { onClickTag(synonym) }) {
 
 
         Text(
             text = synonym,
             style = MaterialTheme.typography.overline,
             textAlign = TextAlign.Center,
-            modifier = Modifier.padding(5.dp)
-        )
-    }
 
+            modifier = Modifier.padding(8.dp)
+        )
+    }*/
+
+    Surface(
+        elevation = 8.dp,
+        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier
+                .border(
+                    width = 1.5.dp,
+                    color = MaterialTheme.colors.primary,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .clickable { onClickTag(synonym) }
+    ) {
+
+        Row() {
+            Text(
+                text = synonym,
+                style = MaterialTheme.typography.overline,
+                textAlign = TextAlign.Center,
+
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
 
 }
