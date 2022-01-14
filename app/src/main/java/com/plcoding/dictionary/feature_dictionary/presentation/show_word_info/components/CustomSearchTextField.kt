79efbcdcff5e6595f5da8a7696543e41faa80eeb
Antony <vontonnie@gmail.com>
2022-01-14 20:26:49 +0300

    Extract Search Field into SearchTextField

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/show_word_info/components/CustomSearchTextField.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/show_word_info/components/CustomSearchTextField.kt
@@ -7,7 +7,6 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -24,65 +23,76 @@ fun CustomSearchTextField(
 ) {
 
 
-    Row (modifier = Modifier.fillMaxSize(0.95f)){
-        TextField(
+    Row(modifier = Modifier.fillMaxWidth(0.95f)) {
+
+        SearchTextField(
             value = value,
             onValueChange = onValueChange,
-
-            label = { Text("Search") },
-          keyboardOptions = KeyboardOptions(
-                autoCorrect = true,
-                keyboardType = KeyboardType.Text
-            ),
-
-            leadingIcon = {
-
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = null
-                )
-            },
-
-            trailingIcon = {
-
-                Icon(
-                    imageVector = Icons.Default.Clear,
-                    contentDescription = null,
-                    Modifier.clickable { onClearIconClick() }
-                )
-            },
-
-
-            textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = MaterialTheme.colors.surface,
-                leadingIconColor = MaterialTheme.colors.primary,
-                trailingIconColor = MaterialTheme.colors.primary
-            ),
-
-
-
-            singleLine = true,
-            modifier = Modifier
-                    .padding(8.dp)
-
-
-
-    
-
-
+            onClearIconClick = onClearIconClick
         )
-        
-
     }
 
 
 }
 
+
+@Composable
+fun SearchTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    onClearIconClick: () -> Unit
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+
+        label = { Text("Search") },
+        keyboardOptions = KeyboardOptions(
+            autoCorrect = true,
+            keyboardType = KeyboardType.Text
+        ),
+
+        leadingIcon = {
+
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+
+        trailingIcon = {
+
+            Icon(
+                imageVector = Icons.Default.Clear,
+                contentDescription = null,
+                Modifier.clickable { onClearIconClick() }
+            )
+        },
+
+
+        textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface,
+            leadingIconColor = MaterialTheme.colors.primary,
+            trailingIconColor = MaterialTheme.colors.primary
+        ),
+
+
+        singleLine = true,
+        modifier = Modifier
+                .padding(8.dp)
+
+
+    )
+
+
+}
+
+
 @Preview(name = "TextPreview")
 @Composable
 fun MyTextFieldPreview() {
-    Column (modifier = Modifier.fillMaxWidth()){
+    Column(modifier = Modifier.fillMaxWidth()) {
         Row {
             TextField(
                 value = "Semi",
