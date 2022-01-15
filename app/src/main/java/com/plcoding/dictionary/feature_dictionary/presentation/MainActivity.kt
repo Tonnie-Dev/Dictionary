817efb3e7fcf168a0e5e8db35a32dacda827b317
Antony <vontonnie@gmail.com>
2022-01-15 09:12:36 +0300

    Inject app in the MainActivity

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/MainActivity.kt
@@ -6,21 +6,27 @@ import androidx.activity.compose.setContent
 
 
 import androidx.compose.ui.ExperimentalComposeUiApi
+import com.plcoding.dictionary.DictionaryApp
 
 import com.plcoding.dictionary.feature_dictionary.presentation.show_word_info.WordInfoScreen
 import com.plcoding.dictionary.feature_dictionary.presentation.ui.theme.DictionaryTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 @ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
+
+    //Main Activity has an empty constructor, field injection is done
+    @Inject
+    lateinit var app:DictionaryApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
 
-         DictionaryTheme {
+         DictionaryTheme(darkTheme = app.isDark) {
 
              WordInfoScreen()
 
