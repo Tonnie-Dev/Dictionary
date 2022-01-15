827e9cfebb2b2da7c5cde1987d0e1be64255b71e
Antony <vontonnie@gmail.com>
2022-01-15 08:52:27 +0300

    Create an observable state in the Application context

--- app/src/main/java/com/plcoding/dictionary/DictionaryApp.kt
+++ app/src/main/java/com/plcoding/dictionary/DictionaryApp.kt
@@ -1,11 +1,15 @@
 package com.plcoding.dictionary
 
 import android.app.Application
+import androidx.compose.runtime.mutableStateOf
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
 class DictionaryApp : Application(){
+
+    //create an observable mutable state
+    val isDark = mutableStateOf(false)
 
     override fun onCreate() {
         super.onCreate()
