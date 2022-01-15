5af1cd41cce4a4c299b304026246323127f8e995
Antony <vontonnie@gmail.com>
2022-01-15 15:05:03 +0300

    Add ConstraintLayout Library

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/show_word_info/components/ThemeTogIconButton.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/presentation/show_word_info/components/ThemeTogIconButton.kt
@@ -1,5 +1,6 @@
 package com.plcoding.dictionary.feature_dictionary.presentation.show_word_info.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -7,20 +8,26 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 
 @Composable
 fun ThemeTogIconButton(onThemeToggle: () -> Unit, modifier: Modifier = Modifier) {
-
-    IconButton(onClick = { onThemeToggle() }, modifier.fillMaxWidth()) {
-
-        Icon(
-
-            imageVector = Icons.Default.Settings,
-            /*painter = painterResource(id = R.drawable.tog_theme),*/
-            contentDescription = null
-        )
+    ConstraintLayout {
 
     }
+
+        IconButton(onClick = { onThemeToggle() }, modifier) {
+
+            Icon(
+
+                imageVector = Icons.Default.Settings,
+                /*painter = painterResource(id = R.drawable.tog_theme),*/
+                contentDescription = null
+            )
+
+        }
+
 
 
 }
